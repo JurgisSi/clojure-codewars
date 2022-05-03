387ca6be60355f54e4e541b8a5d6285eae9064c7
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2022-05-03 23:49:23 +0300

    feat: optimize direction reduction

--- directions-reduction/src/directions_reduction/core.clj
+++ directions-reduction/src/directions_reduction/core.clj
@@ -6,27 +6,15 @@
                  "EAST" "WEST"
                  "WEST" "EAST"))
 
-(defn reduce-directions
-  ([directions one]
-   (conj directions one))
-  ([directions one two]
-   (if (= (get opposites one) two)
-     (not-empty directions)
-     (conj directions one two))
-   )
-  ([directions one two & other]
-   (if (= (get opposites one) two)
-     (if (empty? directions)
-       (apply reduce-directions directions other)
-       (apply reduce-directions (pop directions) (peek directions) other)
-       )
-     (apply reduce-directions (conj directions one) two other))
-   )
+(defn simplify
+  [arr direction]
+  (if (= (peek arr) (get opposites direction))
+    (pop arr)
+    (conj arr direction)
+    )
   )
 
 (defn dirReduc
   [arr]
-  (if (empty? arr)
-    nil
-    (apply reduce-directions [] arr))
+  (not-empty (reduce simplify [] arr))
   )
